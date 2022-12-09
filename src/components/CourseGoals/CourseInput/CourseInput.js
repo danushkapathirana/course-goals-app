e299164e4c5d918be3d08a51bb19b093f2a9d754
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2022-12-09 14:21:57 +0530

    add css classes dynamically to CourseInput with css modules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,44 +1,44 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button'
 
-import "./CourseInput.css"
+import styles from "./CourseInput.module.css"
 
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-& label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-}
+// & label {
+//   font-weight: bold;
+//   display: block;
+//   margin-bottom: 0.5rem;
+// }
 
-& input {
-  display: block;
-  width: 100%;
-  border: 1px solid #ccc;
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-}
+// & input {
+//   display: block;
+//   width: 100%;
+//   border: 1px solid #ccc;
+//   font: inherit;
+//   line-height: 1.5rem;
+//   padding: 0 0.25rem;
+// }
 
-& input:focus {
-  outline: none;
-  background: #fad0ec;
-  border-color: #8b005d;
-}
+// & input:focus {
+//   outline: none;
+//   background: #fad0ec;
+//   border-color: #8b005d;
+// }
 
-&.invalid input {
-  border-color: red;
-  background-color: #ffd7d7;
-}
+// &.invalid input {
+//   border-color: red;
+//   background-color: #ffd7d7;
+// }
 
-&.invalid label {
-  color: red;
-}
+// &.invalid label {
+//   color: red;
+// }
 
-`
+// `
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("")
@@ -62,25 +62,13 @@ const CourseInput = (props) => {
 
   return(
     <form onSubmit={formSubmitHandler}>
-      {/* <FormControl invalid={!isValid}></FormControl> method-02 */}
-      <FormControl className={!isValid && "invalid"}> {/**setting css classes dynamically with styled components; method-01 */}
+      <div className={`${styles["form-control"]} ${!isValid && styles.invalid}`}> {/**setting css classes dynamically with css modules */}
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   )
 }
 
 export default CourseInput
-
-// method-02
-
-// & input {
-//   display: block;
-//   width: 100%;
-//   border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
-//   font: inherit;
-//   line-height: 1.5rem;
-//   padding: 0 0.25rem;
-// }
